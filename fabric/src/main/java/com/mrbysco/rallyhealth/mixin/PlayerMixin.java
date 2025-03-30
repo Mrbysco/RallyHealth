fe7cce3e230b5db65c3f363fd44c11d3e590ed9a
Mrbysco <Mrbysco@users.noreply.github.com>
2025-03-30 17:26:14 +0200

    Port to 1.21.4

--- fabric/src/main/java/com/mrbysco/rallyhealth/mixin/PlayerMixin.java
+++ fabric/src/main/java/com/mrbysco/rallyhealth/mixin/PlayerMixin.java
@@ -1,6 +1,7 @@
 package com.mrbysco.rallyhealth.mixin;
 
 import com.mrbysco.rallyhealth.callback.LivingCallback;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
@@ -14,20 +15,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(Player.class)
 public class PlayerMixin {
 
-	@Inject(method = "actuallyHurt(Lnet/minecraft/world/damagesource/DamageSource;F)V", at = @At(
+	@Inject(method = "actuallyHurt(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/damagesource/DamageSource;F)V", at = @At(
 			value = "INVOKE",
 			target = "Lnet/minecraft/world/entity/player/Player;getDamageAfterArmorAbsorb(Lnet/minecraft/world/damagesource/DamageSource;F)F",
 			shift = At.Shift.BEFORE,
 			ordinal = 0), cancellable = true)
-	public void actuallyHurt(DamageSource source, float amount, CallbackInfo ci) {
-		if (LivingCallback.HURT_EVENT.invoker().hurt((LivingEntity) (Object) this, source, amount) == InteractionResult.FAIL) {
+	public void actuallyHurt(ServerLevel serverLevel, DamageSource damageSource, float amount, CallbackInfo ci) {
+		if (LivingCallback.HURT_EVENT.invoker().hurt((LivingEntity) (Object) this, damageSource, amount) == InteractionResult.FAIL) {
 			ci.cancel();
 		}
 	}
 
-	@Inject(method = "hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z", at = @At("HEAD"), cancellable = true)
-	public void hurt(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
-		if (LivingCallback.ATTACK_EVENT.invoker().attack((LivingEntity) (Object) this, source, amount) == InteractionResult.FAIL) {
+	@Inject(method = "hurtServer(Lnet/minecraft/server/level/ServerLevel;Lnet/minecraft/world/damagesource/DamageSource;F)Z", at = @At("HEAD"), cancellable = true)
+	public void hurt(ServerLevel serverLevel, DamageSource damageSource, float amount, CallbackInfoReturnable<Boolean> cir) {
+		if (LivingCallback.ATTACK_EVENT.invoker().attack((LivingEntity) (Object) this, damageSource, amount) == InteractionResult.FAIL) {
 			cir.setReturnValue(false);
 		}
 	}
